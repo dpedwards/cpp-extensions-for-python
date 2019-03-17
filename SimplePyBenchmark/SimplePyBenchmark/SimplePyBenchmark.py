69ba9429f6e3046d97adbb56166b661741499896
Davain Pablo Edwards <core8@gmx.net>
2019-03-17 02:09:52 +0100

    :star: add python function to call exported DLL methods

--- SimplePyBenchmark/SimplePyBenchmark/SimplePyBenchmark.py
+++ SimplePyBenchmark/SimplePyBenchmark/SimplePyBenchmark.py
@@ -1,4 +1,3 @@
-
 from itertools import islice
 from random import random
 from time import perf_counter
@@ -8,17 +7,22 @@ DATA = list(islice(iter(lambda: (random() - 0.5) * 3.0, None), COUNT))
 
 e = 2.7182818284590452353602874713527
 
+
 def sinh(x):
     return (1 - (e ** (-2 * x))) / (2 * (e ** -x))
 
+
 def cosh(x):
     return (1 + (e ** (-2 * x))) / (2 * (e ** -x))
+
 
 def tanh(x):
     tanh_x = sinh(x) / cosh(x)
     return tanh_x
 
+
 def test(fn, name):
+
     start = perf_counter()
     result = fn(DATA)
     duration = perf_counter() - start
@@ -27,7 +31,11 @@ def test(fn, name):
     for d in result:
         assert -1 <= d <= 1, " incorrect values"
 
+
 if __name__ == "__main__":
     print('Running benchmarks with COUNT = {}'.format(COUNT))
 
     test(lambda d: [tanh(x) for x in d], '[tanh(x) for x in d] (Python implementation)')
+    
+    from FastCppExtension import fast_tanh
+    test(lambda d: [fast_tanh(x) for x in d], '[fast_tanh(x) for x in d] (CPython C++ extension)')
